1764a211574bc2b15d5050fa1461488a8cafd7f7
ThomasWaytec <thomas.waytec@gmail.com>
2024-01-30 23:45:29 +0100

    send the emails

--- mail_bomber.py
+++ mail_bomber.py
@@ -14,12 +14,12 @@ from email_accounts import ACCOUNTS
 
 
 def send_email(session: SMTP_SSL, sender: str, recipients: list[str],
-               content: MIMEText) -> None:
+               mime_content: MIMEText) -> None:
     """
     Send an email using the provided SMTP_SSL session.
     """
 
-    session.sendmail(sender, recipients, content.as_string())
+    session.sendmail(sender, recipients, mime_content.as_string())
 
 
 
@@ -31,11 +31,16 @@ email_content = EmailContent()
 account = choice(ACCOUNTS)
 
 # set up e-mail content
-msg = MIMEText(email_content.body)
-msg['Subject'] = email_content.subject
-msg['From'] = email_content.sender
-msg['To'] = ', '.email_content.recipients
+mime_content = MIMEText(email_content.body)
+mime_content['Subject'] = email_content.subject
+mime_content['From'] = email_content.sender
+mime_content['To'] = ', '.email_content.recipients
 
 # set up SMTP session
 session = SMTP_SSL('smtp.gmail.com', SMTP_SSL_PORT)
 session.login(account.address, account.password)
+
+for _ in range(no_of_emails):
+    send_email(session, account.address, email_content.recipients,
+               mime_content)
+    print("E-mail sent succesfully.")
