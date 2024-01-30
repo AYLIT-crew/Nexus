55c87972fe8f80f578858fcc4a9c2b833908b78c
ThomasWaytec <thomas.waytec@gmail.com>
2024-01-30 21:16:32 +0100

    add hardcoded account information

---
+++ email_accounts.py
@@ -0,0 +1,9 @@
+from email_account import EmailAccount
+
+ACCOUNTS = {
+
+    EmailAccount(address="servicerender100@gmail.com", password="nytd nuae zeed kbkq"),
+    EmailAccount(address="servicerender200@gmail.com", password="uogr hlfp gpxl vmln"),
+}
+
+
