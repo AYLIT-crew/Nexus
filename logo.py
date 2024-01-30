fa2c03f1932c99e592a18745900b955b218c052a
ThomasWaytec <thomas.waytec@gmail.com>
2024-01-31 00:52:26 +0100

    remove new line after logo

--- logo.py
+++ logo.py
@@ -10,7 +10,6 @@ def print_logo() -> None:
     print(cl('-'*30, 'red'))
     print(cl(ff('Nexus')+'\n\t-A mail bomber program.\n\t-An AYLIT Production.\n\t-v1.0', 'red'))
     print(cl('-'*30, 'red'))
-    print('\n')
 
     
 
