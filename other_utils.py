ee4c6cdf86c117cce5e0e76549c0c913217ee2cc
ThomasWaytec <thomas.waytec@gmail.com>
2024-01-31 01:06:05 +0100

    add join_with_and

--- other_utils.py
+++ other_utils.py
@@ -5,3 +5,7 @@ def clear_screen() -> None:
     system("cls||clear")
 
 
+def join_with_and(lst: list[str]) -> str:
+    if len(lst) == 1: return lst[0]
+
+    return ', '.join(lst[:-1]) + ' and ' + lst[-1]
